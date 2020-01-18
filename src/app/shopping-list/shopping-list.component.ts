96c770ce023646e5bf50894f4e71387125a3bfb6
I516409 <rohan.soni@sap.com>
2020-01-19 00:18:59 +0530

    shopping-list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Ingredient} from '../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients:Ingredient[]=[
+    new Ingredient('Cabbage',1),
+    new Ingredient('Onion',1),
+  ];
   constructor() { }
 
   ngOnInit() {
