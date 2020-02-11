0a1bfb1d6dae7e03cc17c6aa2f42e6f13333cf9f
I516409 <rohan.soni@sap.com>
2020-02-11 18:37:11 +0530

    enhancement

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output ,EventEmitter} from '@angular/core';
 import { Recipe } from '../../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-item',
@@ -10,7 +11,11 @@ export class RecipeItemComponent implements OnInit {
 
   constructor() { }
   @Input() recipe:Recipe;
+  @Output() recipeSelected= new EventEmitter<void>();
   ngOnInit() {
+  }
+  onSelected(){
+    this.recipeSelected.emit();
   }
 
 }
