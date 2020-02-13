b265f086f99438b7a9390c5d23588f6cae0d32c6
I516409 <rohan.soni@sap.com>
2020-02-13 18:20:35 +0530

    sl added

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,5 +1,14 @@
-export class Recipe{
-constructor(public name: string,public description: string,public imagePath: string){
+import { Ingredient } from '../shared/ingredient.model';
 
+export class Recipe{
+    public name: string;
+    public description: string;
+    public imagePath: string
+    public ingredients:Ingredient[]
+constructor( name: string, description: string, imagePath: string, ingredients:Ingredient[]){
+    this.name=name;
+    this.description=description;
+    this.imagePath=imagePath;
+    this.ingredients=ingredients;
 }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,18 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService{
     recipeSelected=new EventEmitter<Recipe>();
     recipes:Recipe[]=[
-        new Recipe('A test Recipe','Some Test','https://live.staticflickr.com/5496/31479301445_cb53c0f4e9_b.jpg'),
-        new Recipe('A test Recipe','Some Test','https://live.staticflickr.com/5496/31479301445_cb53c0f4e9_b.jpg'),
+        new Recipe('Chilly Chicken','Some Test','https://live.staticflickr.com/5496/31479301445_cb53c0f4e9_b.jpg',[
+            new Ingredient("Meat",1),
+            new Ingredient("Peanuts",100),
+        ]),
+        new Recipe('Bread','Some Test','https://live.staticflickr.com/5496/31479301445_cb53c0f4e9_b.jpg',[
+            new Ingredient("Bread",1),
+            new Ingredient("Peanuts Butter",1),
+        ]),
     
       ];
       getRecipe(){
